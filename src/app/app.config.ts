3d26b99cdc825a8fef37a7f4c8f0a0f295f2a2de
SimeDev <simedev92@gmail.com>
2025-06-02 10:43:40 +0200

    Providing httpclient from Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
   // HashStrategy
   {
     provide: LocationStrategy,
