489e3923b9c0b51136a81f7abdf1fd5eb17f6500
SimeDev <simedev92@gmail.com>
2025-06-02 10:35:37 +0200

    Setting hash strategy netlify

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+  // HashStrategy
+  {
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  },
 
-    provideHttpClient(withFetch()),
   ],
 };
